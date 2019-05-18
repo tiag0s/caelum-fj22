bbabf7a8e1e04599234ba26f2fc812ed143bb077
Tiago <contato@Tiago.org>
2019-05-18 16:30:35 -0300

    mensagem explicando o trabalho que foi feito

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -3,6 +3,12 @@ package br.com.caelum.ingresso.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+
+public class SessaoController {
 
 @Autowired
 private SalaDao salaDao;
@@ -10,19 +16,20 @@ private SalaDao salaDao;
 @Autowired
 private FilmeDao filmeDao;
 
+
 @GetMapping("/admin/sessao")
 public ModelAndView form(@RequestParam("salaId") Integer salaId) {
 
 	ModelAndView modelAndView = new ModelAndView("sessao/sessao");
 
 	modelAndView.addObject("sala", salaDao.findOne(salaId));
-modelAndView.addObject("filmes", filmeDao.findAll());
+	modelAndView.addObject("filmes", filmeDao.findAll());
 return modelAndView;
 }
 }
 
 
 
-public class SessaoController {
 
-}
+
+
